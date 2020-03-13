ce4ad5518564a8b6b42ef9e6a11a96965b6272f4
bryan <bryandrm97@gmail.com>
2020-03-13 12:20:00 -0600

    Updates

---
+++ precache-manifest.dbf2f33843dabad16e261c82a526e3b5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5dc5c461e5afcca5ae553bd797662fad",
+    "url": "/Bryandrm/Inventory/index.html"
+  },
+  {
+    "revision": "ec381a742113b95d4637",
+    "url": "/Bryandrm/Inventory/static/css/main.61c744dc.chunk.css"
+  },
+  {
+    "revision": "b193f7bdcffaaba84a4f",
+    "url": "/Bryandrm/Inventory/static/js/2.0af04045.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/Bryandrm/Inventory/static/js/2.0af04045.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec381a742113b95d4637",
+    "url": "/Bryandrm/Inventory/static/js/main.22e897b7.chunk.js"
+  },
+  {
+    "revision": "5f530f03ca30a4f1f01e",
+    "url": "/Bryandrm/Inventory/static/js/runtime-main.ba5a377d.js"
+  }
+]);
